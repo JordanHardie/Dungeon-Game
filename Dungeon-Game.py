987ef54ac63c061925bccd6dc375110a30cdf210
JordanHardie <62230359+JordanHardie@users.noreply.github.com>
2020-09-28 22:20:18 +1000

    Quit Update
    + The program quits now!

--- Dungeon-Game.py
+++ Dungeon-Game.py
@@ -1,5 +1,5 @@
 '''
-This program is free software. It comes without any warranty, to
+#This program is free software. It comes without any warranty, to
     * the extent permitted by applicable law. You can redistribute it
     * and/or modify it under the terms of the Do What The Fuck You Want
     * To Public License, Version 2, as published by Sam Hocevar. See
@@ -402,6 +402,10 @@ def Main():
 
             spc()
             turn = 0
+
+            print(quit)
+            spc()
+            quit() 
 
         elif IsDead(enemy) == True:
             print(enemy["Name"] + " died!")
